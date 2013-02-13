adca99668bd90053a33b481d163a453d3a98792d
Martin Affolter <martin@affolter.net>
2013-02-13 14:04:23 +0100

    Config:
    - added fluent config
    - added logging

--- ConfigByFNH/Program.cs
+++ ConfigByFNH/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace ConfigByFNH
+﻿using System;
+using Eg.FluentMappings.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Bytecode;
+
+namespace ConfigByFNH
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var nhConfig = Fluently.Configure()
+                                   .Database(MsSqlConfiguration.MsSql2008.ConnectionString(
+                                       connstr => connstr.FromConnectionStringWithKey("db"))
+                                                               .AdoNetBatchSize(100))
+                                   .ProxyFactoryFactory<DefaultProxyFactoryFactory>()
+                                   .Mappings(mappings => mappings.FluentMappings.AddFromAssemblyOf<ProductMapping>())
+                                   .BuildConfiguration();
+
+            var sessionFactory = nhConfig.BuildSessionFactory();
+            Console.WriteLine("NHibernate configured fluently");
+            Console.ReadKey();
         }
     }
 }
